0a771c6d68d71b1f8c36db96391e39c1a8dda540
s0j097u <s0j097u@homeoffice.wal-mart.com>
2023-09-28 22:27:56 +0530

    added logic for message controller

--- chatsBackend/models/messageModel.js
+++ chatsBackend/models/messageModel.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
-const messageModel = mongoose.Schema(
+const messageSchema = mongoose.Schema(
   {
     sender: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
     content: {type: String, trim: true },
-    chat: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    readBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
   {
     timestamp: true
   }
 )
 
-const Message = mongoose.model("Message",messageModel);
+const Message = mongoose.model("Message",messageSchema);
 module.exports = Message;
